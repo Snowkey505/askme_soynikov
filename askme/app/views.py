24e907fd803e72bfe2e6a143dd03146bd7e84e40
PS <jooonsnow@mail.ru>
2024-11-13 20:52:20 +0300

    corrections

--- askme/app/views.py
+++ askme/app/views.py
@@ -1,6 +1,6 @@
 import copy
 
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.template.response import TemplateResponse
 from django.shortcuts import render
 
@@ -18,17 +18,30 @@ ANSWERS = [f"Sometimes answer is in your knowledge, so you just have to just fin
 TAGS = ["tag_0", "tag_1", "tag_2", "tag_3", "blabla"]
 
 
+def paginate(objects_list, request, per_page=5):
+    page_number = request.GET.get('page', 1)
+    paginator = Paginator(objects_list, per_page)
+
+    try:
+        page = paginator.page(page_number)
+    except PageNotAnInteger:
+        page = paginator.page(1)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+
+    return page
+
+
 def index(request):
-    page_num = request.GET.get('page', 1)
-    paginator = Paginator(QUESTIONS, 5)
-    page = paginator.page(page_num)
+    page = paginate(QUESTIONS, request)
     return render(request, 'index.html', context={'questions': page.object_list, 'page_obj': page, 'tags': TAGS})
 
 
 def hot(request):
     hot_questions = copy.deepcopy(QUESTIONS)
     hot_questions.reverse()
-    return render(request, 'hot.html', context={'questions': hot_questions, 'tags': TAGS})
+    page = paginate(hot_questions, request)
+    return render(request, 'hot.html', context={'questions': page.object_list, 'page_obj': page, 'tags': TAGS})
 
 
 def login(request):
@@ -45,17 +58,13 @@ def signup(request):
 
 def tag(request, tag_name):
     tag_questions = [question for question in QUESTIONS if tag_name in question['tags']]
-    page_num = request.GET.get('page', 1)
-    paginator = Paginator(tag_questions, 5)
-    page = paginator.page(page_num)
+    page = paginate(tag_questions, request)
     return render(request, 'tag.html', context={'questions': page.object_list, 'page_obj': page, 'tag_name': tag_name, 'tags': TAGS})
 
 
 def question(request, question_id):
     question = QUESTIONS[question_id]
-    page_num = request.GET.get('page', 1)
-    paginator = Paginator(ANSWERS, 3)
-    page = paginator.page(page_num)
+    page = paginate(ANSWERS, request)
     return render(request, 'question.html', context={'question': question, 'tags': TAGS, 'answers': page.object_list, 'page_obj': page})
 
 
